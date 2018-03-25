d3ab3a9b66c683233fe70b79d74b33f9c7430878
Burin Choomnuan <agilecreativity@gmail.com>
2018-03-25 00:00:02 -0400

    Update dependency and use better screenshot

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject hn-reader "0.2.1"
+(defproject hn-reader "0.2.2"
   :description "Easy ways to access all of the latest Hacker News links in one page"
   :url "https://github.com/agilecreativity/hn-reader"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev {:dependencies [[lein-bin "0.3.5"]
-                                  [com.jakemccrary/lein-test-refresh "0.19.0"]]}
+                                  [com.jakemccrary/lein-test-refresh "0.22.0"]]}
              :uberjar {:aot :all}}
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
@@ -12,9 +12,9 @@
         :bin-path "~/bin"
         :bootclasspath true}
   :plugins [[lein-bin "0.3.5"]
-            [lein-cljfmt "0.5.6"]
+            [lein-cljfmt "0.5.7"]
             [lein-auto "0.1.3"]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [reaver "0.1.2"]
                  [org.clojure/tools.cli "0.3.5"]
                  [me.raynes/fs "1.4.6"]]
